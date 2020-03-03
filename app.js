b73bd5ca90ac551af30175d165754bdefd823a9c
creamerr8 <creamerr8@gmail.com>
2020-03-03 14:02:12 -0800

    added promts

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-// var question1 = ('')
-// var question1 = ('')
-// var question1 = ('')
-// var question1 = ('')
-// var question1 = ('')
+var state = promt('Am I from Washington?');
+
+var age = promt('Am I a minor?');
+
+var sports = promt('Am I an athlete?');
+
+var sxhool = promt('Am I in school?');
+
+var game = promt('Am I a gamer?');
